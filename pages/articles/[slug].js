ee50e634a558e7ae2507d6e65d355c2b4da3ee00
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-06-08 16:01:55 +0200

    feat: add image to all blog post

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -1,4 +1,6 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+import Image from 'next/image'
 
 let client = require('contentful').createClient({
     space: process.env.NEXT_CONTENTFUL_SPACE_ID,
@@ -40,6 +42,15 @@ export default function Article({ article }) {
     console.log(article);
     return <div>
         <h1>{article.fields.title}</h1>
-        <div>{documentToReactComponents(article.fields.content)}</div>
+        <div>{documentToReactComponents(article.fields.content, {
+            renderNode: {
+                [BLOCKS.EMBEDDED_ASSET]: node =>
+                    <Image
+                        src={'https:' + node.data.target.fields.file.url}
+                        width={node.data.target.fields.file.details.image.width}
+                        height={node.data.target.fields.file.details.image.height}
+                    />
+            }
+        })}</div>
     </div>;
 }
